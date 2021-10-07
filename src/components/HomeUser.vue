84e1070409a1c05e46d9733e167e4103a397be30
teezziestezos <teezziestezos@penguin>
2021-10-07 12:33:14 +0900

    vue4-3

---
+++ src/components/HomeUser.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="homeuser">
+    <h1>{{ user }}{{ msg }}</h1>
+    <p>残高：{{ balance }}</p>
+    <footer>Copyright ©2021 XX Inc All rights reserved.</footer>
+  </div>
+</template>
+<script src="https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js"></script>
+<script src="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.js"></script>
+<script>
+
+  export default {
+    name: 'HomeUser',
+    data() {
+    return {
+      msg: 'さんようこそ',
+    }
+    },
+  computed:{
+    user:{
+        get(){
+            return this.$store.getters.username;
+        }
+    },
+    balance:{
+        get(){
+            return this.$store.getters.balance;
+        }
+    }
+  }
+}
+</script>
+<style scoped>
+template {
+    margin: 0;
+    padding: 0;
+}
+h1 {
+  text-align: left;
+}
+p {
+    text-align: right;
+    margin-top: 0;
+}
+footer {
+    text-align: center;
+    padding: 20px 0;
+    margin: 20px 0;
+}
+
+</style>
